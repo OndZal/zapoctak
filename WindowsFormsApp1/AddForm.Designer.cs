d34e73a60e46a3524b99df36b21a496d53841841
OndZal <ondrej.zalesak00@gmail.com>
2019-07-01 21:28:33 +0200

    Added adding turmites from a file

---
+++ WindowsFormsApp1/AddForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class AddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileLoadButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fileLoadButton
+            // 
+            this.fileLoadButton.Location = new System.Drawing.Point(21, 73);
+            this.fileLoadButton.Name = "fileLoadButton";
+            this.fileLoadButton.Size = new System.Drawing.Size(75, 23);
+            this.fileLoadButton.TabIndex = 0;
+            this.fileLoadButton.Text = "Load";
+            this.fileLoadButton.UseVisualStyleBackColor = true;
+            this.fileLoadButton.Click += new System.EventHandler(this.LoadFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(21, 43);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 2;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(21, 13);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 3;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // AddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 111);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.fileLoadButton);
+            this.Name = "AddForm";
+            this.Text = "Adding stuff";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fileLoadButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
---
+++ WindowsFormsApp1/AddForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class AddForm : Form
+    {
+        public AddForm()
+        {
+            InitializeComponent();
+        }
+
+        public Turmite toAdd = null;
+
+        private void LoadFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader file = new StreamReader(openFileDialog1.FileName);
+                int colorCount = 0, stateCount = 0;
+                try
+                {
+                    colorCount = int.Parse(file.ReadLine());
+                    stateCount = int.Parse(file.ReadLine());
+                    toAdd = new Turmite(stateCount, colorCount);
+                    while (!file.EndOfStream)
+                    {
+                        string[] line = file.ReadLine().Split('='); // line format : <current state>,<current color>=<new state>,<new color>,<number of steps>,<turn(0=left,1=none,2=right)>
+                        string[] indices = line[0].Split(',');
+                        int state = int.Parse(indices[0]);
+                        int color = int.Parse(indices[1]);
+                        string[] values = line[1].Split(',');
+                        toAdd.stateTable[state, color] = int.Parse(values[0]);
+                        toAdd.colorTable[state, color] = int.Parse(values[1]);
+                        toAdd.stepTable[state, color] = int.Parse(values[2]);
+                        toAdd.turnTable[state, color]  = (Turns)int.Parse(values[3]);
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("ERROR - Invalid File Format");
+                    file.Close();
+                    toAdd = null;
+                    return;
+                }
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
